790cd6c96f3c9594f77abfe4d5d4322dcaa3eb99
josecheo <joseche0.ja@gmail.com>
2020-08-17 16:31:49 -0500

    que nos hace especial

--- src/module/plans/index.jsx
+++ src/module/plans/index.jsx
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../../components/button";
+import moneyback from "../../assets/services/money-back.svg";
+import moneybackwhite from "../../assets/services/money-back-white.svg";
+import support from "../../assets/services/support.svg";
+import supportwhite from "../../assets/services/supportwite.svg";
+import block from "../../assets/services/block.svg";
+import blockwhite from "../../assets/services/blockwhite.svg";
+import trusted from "../../assets/services/trusted.svg";
+import trustedwhite from "../../assets/services/trustedwhite.svg";
+
 import {
   Wrapper,
   PrincipalContainer,
@@ -15,107 +24,175 @@ import {
   Title,
   WhiteContainer,
   TitleX,
+  EspecialText,
+  EspecialP,
+  EspecialImg,
 } from "./styles";
 
-const Plans = () => (
-  <Wrapper>
-    <PrincipalContainer>
-      <Text>Infraestructura cloud </Text>
-      <P>Los precios son de acuerdo al requerimiento del cliente.</P>
-      <Line />
-      <ContainerCards width="70%">
-        {/* PLAN BASICO */}
-        <PlanCard background="-webkit-linear-gradient(90deg, #ffba75, #ff7f00)">
-          <PlandCardContainer>
-            <Title background="rgba(255, 186, 117, 0.4)">PLAN BÁSICO</Title>
-            <P>✔ Excelente para Iniciar</P>
-            <P>✔ 100% seguro</P>
-            <P>✔ Soporte 24/7</P>
-          </PlandCardContainer>
-          <WhiteContainer>
-            <TitleX>Seguridad PFSENSE</TitleX>
-            <TitleX>Correo zima</TitleX>
-            <TitleX>Nextcloud</TitleX>
-            <TitleX>Talk</TitleX>
-            <TitleX>Videollamada</TitleX>
-            <TitleX>Document Server</TitleX>
-            <Button
-              mt="30px"
-              width="100%"
-              height="48px"
-              text="Cotiza ahora"
-              background="-webkit-linear-gradient(90deg, #ffba75, #ff7f00)"
-              color="#FFFFFF"
-            ></Button>
-          </WhiteContainer>
+const Plans = () => {
+  const [hoverMoney, setHoverMoney] = useState(false);
+  const [hoverSupport, setHoverSupport] = useState(false);
+  const [hoverBlock, setHoverBlock] = useState(false);
+  const [hoverTrusted, setHoverTrusted] = useState(false);
+  return (
+    <Wrapper>
+      <PrincipalContainer>
+        <Text>Infraestructura cloud </Text>
+        <P>Los precios son de acuerdo al requerimiento del cliente.</P>
+        <Line />
+        <ContainerCards width="70%">
+          {/* PLAN BASICO */}
+          <PlanCard background="-webkit-linear-gradient(90deg, #ffba75, #ff7f00)">
+            <PlandCardContainer>
+              <Title background="rgba(255, 186, 117, 0.4)">PLAN BÁSICO</Title>
+              <P>✔ Excelente para Iniciar</P>
+              <P>✔ 100% seguro</P>
+              <P>✔ Soporte 24/7</P>
+            </PlandCardContainer>
+            <WhiteContainer>
+              <TitleX>Seguridad PFSENSE</TitleX>
+              <TitleX>Correo zima</TitleX>
+              <TitleX>Nextcloud</TitleX>
+              <TitleX>Talk</TitleX>
+              <TitleX>Videollamada</TitleX>
+              <TitleX>Document Server</TitleX>
+              <Button
+                mt="30px"
+                width="100%"
+                height="48px"
+                text="Cotiza ahora"
+                background="-webkit-linear-gradient(90deg, #ffba75, #ff7f00)"
+                color="#FFFFFF"
+              ></Button>
+            </WhiteContainer>
 
-          <Shape></Shape>
-        </PlanCard>
-        {/* PLAN ESTANDAR */}
-        <PlanCard background="-webkit-linear-gradient(90deg, #9c8df1, #7f29f0)">
-          <PlandCardContainer>
-            <Title background="rgba(156, 141, 241, 0.4)">PLAN ESTANDAR</Title>
-            <P>✔ Excelente para negocios</P>
-            <P>✔ 100% seguro</P>
-            <P>✔ Soporte 24/7</P>
-          </PlandCardContainer>
-          <WhiteContainer>
-            <TitleX>Seguridad PFSENSE</TitleX>
-            <TitleX>Correo zima</TitleX>
-            <TitleX>Nextcloud</TitleX>
-            <TitleX>Talk</TitleX>
-            <TitleX>Videollamada</TitleX>
-            <TitleX>Document Server</TitleX>
-            <Button
-              mt="30px"
-              width="100%"
-              height="48px"
-              text="Cotiza ahora"
-              background="-webkit-linear-gradient(90deg, #9c8df1, #7f29f0)"
-              color="#FFFFFF"
-            ></Button>
-          </WhiteContainer>
+            <Shape></Shape>
+          </PlanCard>
+          {/* PLAN ESTANDAR */}
+          <PlanCard background="-webkit-linear-gradient(90deg, #9c8df1, #7f29f0)">
+            <PlandCardContainer>
+              <Title background="rgba(156, 141, 241, 0.4)">PLAN ESTANDAR</Title>
+              <P>✔ Excelente para negocios</P>
+              <P>✔ 100% seguro</P>
+              <P>✔ Soporte 24/7</P>
+            </PlandCardContainer>
+            <WhiteContainer>
+              <TitleX>Seguridad PFSENSE</TitleX>
+              <TitleX>Correo zima</TitleX>
+              <TitleX>Nextcloud</TitleX>
+              <TitleX>Talk</TitleX>
+              <TitleX>Videollamada</TitleX>
+              <TitleX>Document Server</TitleX>
+              <Button
+                mt="30px"
+                width="100%"
+                height="48px"
+                text="Cotiza ahora"
+                background="-webkit-linear-gradient(90deg, #9c8df1, #7f29f0)"
+                color="#FFFFFF"
+              ></Button>
+            </WhiteContainer>
 
-          <Shape></Shape>
-        </PlanCard>
-        {/* PLAN premiun */}
-        <PlanCard background="-webkit-linear-gradient(90deg, #47f48f, #0dcf77)">
-          <PlandCardContainer>
-            <Title background="rgba(71, 244, 143, 0.4)">PLAN PREMIUM</Title>
-            <P>✔ Para grandes empresas</P>
-            <P>✔ 100% seguro</P>
-            <P>✔ Soporte 24/7</P>
-          </PlandCardContainer>
-          <WhiteContainer>
-            <TitleX>Seguridad PFSENSE</TitleX>
-            <TitleX>Correo zima</TitleX>
-            <TitleX>Nextcloud</TitleX>
-            <TitleX>Talk</TitleX>
-            <TitleX>Videollamada</TitleX>
-            <TitleX>Document Server</TitleX>
-            <Button
-              mt="30px"
-              width="100%"
-              height="48px"
-              text="Cotiza ahora"
-              background="-webkit-linear-gradient(90deg, #47f48f, #0dcf77)"
-              color="#FFFFFF"
-            ></Button>
-          </WhiteContainer>
+            <Shape></Shape>
+          </PlanCard>
+          {/* PLAN premiun */}
+          <PlanCard background="-webkit-linear-gradient(90deg, #47f48f, #0dcf77)">
+            <PlandCardContainer>
+              <Title background="rgba(71, 244, 143, 0.4)">PLAN PREMIUM</Title>
+              <P>✔ Para grandes empresas</P>
+              <P>✔ 100% seguro</P>
+              <P>✔ Soporte 24/7</P>
+            </PlandCardContainer>
+            <WhiteContainer>
+              <TitleX>Seguridad PFSENSE</TitleX>
+              <TitleX>Correo zima</TitleX>
+              <TitleX>Nextcloud</TitleX>
+              <TitleX>Talk</TitleX>
+              <TitleX>Videollamada</TitleX>
+              <TitleX>Document Server</TitleX>
+              <Button
+                mt="30px"
+                width="100%"
+                height="48px"
+                text="Cotiza ahora"
+                background="-webkit-linear-gradient(90deg, #47f48f, #0dcf77)"
+                color="#FFFFFF"
+              ></Button>
+            </WhiteContainer>
 
-          <Shape></Shape>
-        </PlanCard>
-      </ContainerCards>
-      <TextX>¿Qué nos hace especial? </TextX>
-      <Line />
-      <ContainerCards width="80%">
-        <EspecialCard></EspecialCard>
-        <EspecialCard></EspecialCard>
-        <EspecialCard></EspecialCard>
-        <EspecialCard></EspecialCard>
-      </ContainerCards>
-    </PrincipalContainer>
-  </Wrapper>
-);
+            <Shape></Shape>
+          </PlanCard>
+        </ContainerCards>
+        <TextX>¿Qué nos hace especial? </TextX>
+        <Line />
+        <ContainerCards width="80%">
+          <EspecialCard
+            onMouseOver={() => setHoverMoney(true)}
+            onMouseLeave={() => setHoverMoney(false)}
+          >
+            {hoverMoney ? (
+              <EspecialImg src={moneybackwhite}></EspecialImg>
+            ) : (
+              <EspecialImg src={moneyback}></EspecialImg>
+            )}
+            <EspecialText>Garantía de reembolso </EspecialText>
+            <EspecialP>
+              Pero el precio más grande es gratis, pero tal como está. Un engaño
+              de baloncesto altamente profesional.
+            </EspecialP>
+          </EspecialCard>
+
+          <EspecialCard
+            onMouseOver={() => setHoverSupport(true)}
+            onMouseLeave={() => setHoverSupport(false)}
+          >
+            {hoverSupport ? (
+              <EspecialImg src={supportwhite}></EspecialImg>
+            ) : (
+              <EspecialImg src={support}></EspecialImg>
+            )}
+            <EspecialText>Mejor soporte </EspecialText>
+            <EspecialP>
+              Pero el precio más grande es gratis, pero tal como está. Un engaño
+              de baloncesto altamente profesional.
+            </EspecialP>
+          </EspecialCard>
+
+          <EspecialCard
+            onMouseOver={() => setHoverBlock(true)}
+            onMouseLeave={() => setHoverBlock(false)}
+          >
+            {hoverBlock ? (
+              <EspecialImg src={blockwhite}></EspecialImg>
+            ) : (
+              <EspecialImg src={block}></EspecialImg>
+            )}
+            <EspecialText>Seguridad de primero </EspecialText>
+            <EspecialP>
+              Pero el precio más grande es gratis, pero tal como está. Un engaño
+              de baloncesto altamente profesional.
+            </EspecialP>
+          </EspecialCard>
+
+          <EspecialCard
+            onMouseOver={() => setHoverTrusted(true)}
+            onMouseLeave={() => setHoverTrusted(false)}
+          >
+            {hoverTrusted ? (
+              <EspecialImg src={trustedwhite}></EspecialImg>
+            ) : (
+              <EspecialImg src={trusted}></EspecialImg>
+            )}
+            <EspecialText>Confianza 100% </EspecialText>
+            <EspecialP>
+              Pero el precio más grande es gratis, pero tal como está. Un engaño
+              de baloncesto altamente profesional.
+            </EspecialP>
+          </EspecialCard>
+        </ContainerCards>
+      </PrincipalContainer>
+    </Wrapper>
+  );
+};
 
 export default Plans;
--- src/module/plans/styles.js
+++ src/module/plans/styles.js
@@ -153,16 +153,49 @@ export const WhiteContainer = styled.div`
 
 export const EspecialCard = styled.div`
   display: flex;
-  border-radius: 20px;
-  padding: 20px 20px 20px 0px;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  padding: 20px;
   width: 300px;
-  height: 400px;
+  height: 240px;
   background: white;
   margin-top: 20px;
-  box-shadow: 10px 10px 21px -6px rgba(0, 0, 0, 0.42);
-  transition: transform 0.4s;
-  cursor: pointer;
-  &:hover {
-    transform: scale(1.08);
+  border-radius: 30px;
+  transition: background 0.4s;
+  &:hover p {
+    color: ${(props) => props.theme.colors.white};
   }
+  &:hover h2 {
+    color: ${(props) => props.theme.colors.white};
+  }
+  &:hover {
+    background: ${(props) => props.theme.colors.primary};
+  }
+  &:hover img {
+    transform: scale(1.3);
+  }
+`;
+
+export const EspecialText = styled.h2`
+  transition: color 0.4s;
+  font-family: "Montserrat", sans-serif;
+  color: ${(props) => props.theme.colors.semiblack};
+  font-size: 1rem;
+  font-weight: 600;
+`;
+export const EspecialP = styled.p`
+  font-family: "Montserrat", sans-serif;
+  font-weight: medium;
+  font-size: 1rem;
+  transition: color 0.4s;
+  text-align: center;
+  color: ${(props) => props.theme.colors.semiblack};
+  max-width: 100%;
+`;
+
+export const EspecialImg = styled.img`
+  width: 60px;
+  transition: transform 0.4s;
+  margin-bottom: 20px;
 `;
